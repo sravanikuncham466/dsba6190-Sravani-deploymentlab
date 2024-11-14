64a32fb3f7aea5f275fe83f646b5d6c25b322dfb
sravanikuncham466 <163476210+sravanikuncham466@users.noreply.github.com>
2024-11-14 05:57:28 +0530

    new n

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "KS"
+  default     = "ks"
 }
 
 variable "class_name" {
